cf150c32350c8c0461cce4bec33860dd77ba4440
batuogkoc <bgunduz21@ku.edu.tr>
2025-01-20 21:28:17 +0300

    fixed minor issue

--- load_checkpoint.py
+++ load_checkpoint.py
@@ -13,31 +13,32 @@ def load_checkpoint(path):
     model = ResNetWrapper(5, 62, "resnet-101")
     model.load_state_dict(checkpoint["model_state_dict"])
 
-    dataset = CaptchaDataset(
-        "akashguna/large-captcha-dataset",
-        transform=T.Compose((T.Resize((224, 224)), T.ToTensor())),
-    )
-    loader = DataLoader(dataset, 1, shuffle=False)
-
     loss_fn = torch.nn.CrossEntropyLoss()
-    return model, loader, loss_fn
+    return model, loss_fn
 
 
 if __name__ == "__main__":
     PATH = "cleaned_checkpoint.pt"
 
-    model, loader, loss_fn = load_checkpoint(PATH)
+    model, loss_fn = load_checkpoint(PATH)
 
     # user code
+    dataset = CaptchaDataset(
+        "akashguna/large-captcha-dataset",
+        transform=T.Compose((T.Resize((224, 224)), T.ToTensor())),
+    )
+    loader = DataLoader(dataset, batch_size=1, shuffle=False)
     x, y = next(iter(loader))
-    x.set_requires_grad(True)
+    x.requires_grad = True
+    print(x.requires_grad)
     y_pred = model(x)
     loss = loss_fn(y_pred, y)
+    model.zero_grad()
     loss.backward()
-    x_grad = x.grad
+    print(x.grad)
 
-    x_perturbed = x - 0.1 * x.grad
+    # x_perturbed = x - 0.1 * x.grad
 
-    y_perturbed_pred = model(x_perturbed)
+    # y_perturbed_pred = model(x_perturbed)
 
-    print(y, torch.argmax(y_pred, 1), torch.argmax(y_perturbed_pred, 1))
+    # print(y, torch.argmax(y_pred, 1), torch.argmax(y_perturbed_pred, 1))
