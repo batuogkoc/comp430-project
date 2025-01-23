85a8dc44df998a78af1c58d25deca91da57a3fbe
batuogkoc <bgunduz21@ku.edu.tr>
2025-01-23 21:01:29 +0300

    printed fgsm better

--- fast_gradient_sign.py
+++ fast_gradient_sign.py
@@ -25,38 +25,55 @@ def fast_gradient_sign(model, x, y, epsilon=0.1):
     return torch.clamp(x_perturbed, 0, 1)
 
 
-def visualize(model, loader, num, epsilon, device):
+def visualize(model, loader, num, epsilons, device):
     model.eval()
     model.to(device)
-    fig, axs = plt.subplots(num, 2, figsize=(15, 18))
-    fig.subplots_adjust(wspace=0)
+    fig, axs = plt.subplots(num, len(epsilons) + 1, figsize=(10, 9.5))
+    fig.subplots_adjust(wspace=0, hspace=0)
+    axs[num - 1, 0].set_xlabel(f"Epsilon: {0}")
     for i, (x, y) in enumerate(loader):
         if i >= num:
             break
         x = x.to(device)
         y = y.to(device)
-        x_perturbed = x.clone().detach().requires_grad_(True)
-        y_pred = model(x_perturbed)
-
-        x_perturbed = fast_gradient_sign(model, x_perturbed, y, epsilon=epsilon)
-        y_perturbed_pred = model(x_perturbed)
-
+        x = x.clone().detach().requires_grad_(True)
+        y_pred = model(x)
         y_str = digits_to_label(y[0])
         y_pred_str = digits_to_label(torch.argmax(y_pred, 1)[0])
-        y_perturbed_pred_str = digits_to_label(torch.argmax(y_perturbed_pred, 1)[0])
-        # print("gt:", y_str)
-        # print("pred:", y_pred_str)
-        # print("perturbed:", y_perturbed_pred_str)
-        # print("target:", y_target_str)
-
-        axs[i, 0].set_title(f"actual: {y_str} pred: {y_pred_str}")
+        axs[i, 0].set_title(f"P: {y_pred_str}")
+        axs[i, 0].set_ylabel(f"GT: {y_str}")
         axs[i, 0].imshow(torch.permute(x[0], (1, 2, 0)).detach().cpu().numpy())
-        axs[i, 0].axis("off")
-        axs[i, 1].set_title(f"permuted pred: {y_perturbed_pred_str}")
-        axs[i, 1].imshow(
-            torch.permute(x_perturbed[0], (1, 2, 0)).detach().cpu().numpy()
-        )
-        axs[i, 1].axis("off")
+        # axs[i, 0].axis("off")
+        axs[i, 0].spines["top"].set_visible(False)
+        axs[i, 0].spines["right"].set_visible(False)
+        axs[i, 0].spines["left"].set_visible(False)
+        axs[i, 0].spines["bottom"].set_visible(False)
+        axs[i, 0].set_xticks([])  # Remove x-ticks
+        axs[i, 0].set_yticks([])  # Remove y-ticks
+        for j, epsilon in enumerate(epsilons):
+            x_perturbed = fast_gradient_sign(model, x, y, epsilon=epsilon)
+            y_perturbed_pred = model(x_perturbed)
+
+            y_perturbed_pred_str = digits_to_label(torch.argmax(y_perturbed_pred, 1)[0])
+            # print("gt:", y_str)
+            # print("pred:", y_pred_str)
+            # print("perturbed:", y_perturbed_pred_str)
+            # print("target:", y_target_str)
+
+            axs[i, j + 1].set_title(f"P: {y_perturbed_pred_str}")
+            if i == num - 1:
+                print("a")
+                axs[i, j + 1].set_xlabel(f"Epsilon: {epsilon}")
+            axs[i, j + 1].imshow(
+                torch.permute(x_perturbed[0], (1, 2, 0)).detach().cpu().numpy()
+            )
+            # axs[i, j + 1].axis("off")
+            axs[i, j + 1].spines["top"].set_visible(False)
+            axs[i, j + 1].spines["right"].set_visible(False)
+            axs[i, j + 1].spines["left"].set_visible(False)
+            axs[i, j + 1].spines["bottom"].set_visible(False)
+            axs[i, j + 1].set_xticks([])  # Remove x-ticks
+            axs[i, j + 1].set_yticks([])  # Remove y-ticks
 
     fig.savefig("image")
 
@@ -122,10 +139,13 @@ if __name__ == "__main__":
         [0.8, 0.1, 0.1],
         "combined_splits_indices.cache",
     )
-    test_subset = Subset(test_set, range(100))
-    loader = DataLoader(test_subset, batch_size=16, shuffle=True)
 
-    # visualize(model, test_loader, 4, DEVICE)
+    loader = DataLoader(test_set, batch_size=1, shuffle=True)
+    visualize(model, loader, 4, [1e-2, 0.05, 0.1, 0.2], DEVICE)
+
+    # test_subset = Subset(test_set, range(100))
+    # loader = DataLoader(test_subset, batch_size=16, shuffle=True)
+
     # orig, pert = evaluate_fgsm(model, loader, 0.1, DEVICE)
     # print("orig: ", orig, "pert: ", pert)
-    plot_epsilon_dependency(model, loader, DEVICE)
+    # plot_epsilon_dependency(model, loader, DEVICE)
