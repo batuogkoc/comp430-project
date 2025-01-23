a160a7debc0cd49096a8fc63386cd4a08c6bb947
batuogkoc <bgunduz21@ku.edu.tr>
2025-01-23 20:24:25 +0300

    ran several tests with the fgsm

--- fast_gradient_sign.py
+++ fast_gradient_sign.py
@@ -1,103 +1,131 @@
 import torch
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Subset
 import torchvision.transforms as T
 from train import ResNetWrapper
-from captcha_datasets.datasets import CaptchaDataset, index_to_char, digits_to_label
+from captcha_datasets.datasets import (
+    CaptchaDataset,
+    index_to_char,
+    digits_to_label,
+    get_combined_dataset,
+    split_dataset,
+)
 from matplotlib import pyplot as plt
+from evaluate_performance import load_checkpoint, evaluate_performance
+from tqdm import tqdm
+import numpy as np
 
 
-def load_checkpoint(path):
-    checkpoint = torch.load(path)
-
-    print(checkpoint.keys())
-
-    model = ResNetWrapper(5, 62, "resnet-101")
-    model.load_state_dict(checkpoint["model_state_dict"])
-
-    loss_fn = torch.nn.CrossEntropyLoss()
-    return model, loss_fn
-
-
-def peek_inside_model(model, x):
-    # inputs = model.image_processor(
-    #     x, return_tensors="pt", do_rescale=False, device="cuda"
-    # ).to(x.device)
-    inputs = {
-        "pixel_values": T.Normalize(
-            mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
-        )(x)
-    }
-    # print(torch.allclose)
-    # print(dir(model.image_processor))
-    outputs = model.model(**inputs)
-    # outputs = model.model(pixel_values=x)
-    last_hidden_states = outputs.last_hidden_state
-    flattened_output = model.head(last_hidden_states)
-    return torch.reshape(
-        flattened_output,
-        [flattened_output.shape[0], model.num_chars, model.num_digits],
-    )
-
-
-def fast_gradient_sign(model, x, y):
+def fast_gradient_sign(model, x, y, epsilon=0.1):
     x_perturbed = x.clone().detach().requires_grad_(True)
-    y_pred = peek_inside_model(model, x_perturbed)
+    y_pred = model(x_perturbed)
     loss = loss_fn(y_pred, y)
     model.zero_grad()
     loss.backward()
-    x_perturbed = x_perturbed + 0.1 * torch.sign(x_perturbed.grad)
+    x_perturbed = x_perturbed + epsilon * torch.sign(x_perturbed.grad)
     return torch.clamp(x_perturbed, 0, 1)
 
 
+def visualize(model, loader, num, epsilon, device):
+    model.eval()
+    model.to(device)
+    fig, axs = plt.subplots(num, 2, figsize=(15, 18))
+    fig.subplots_adjust(wspace=0)
+    for i, (x, y) in enumerate(loader):
+        if i >= num:
+            break
+        x = x.to(device)
+        y = y.to(device)
+        x_perturbed = x.clone().detach().requires_grad_(True)
+        y_pred = model(x_perturbed)
+
+        x_perturbed = fast_gradient_sign(model, x_perturbed, y, epsilon=epsilon)
+        y_perturbed_pred = model(x_perturbed)
+
+        y_str = digits_to_label(y[0])
+        y_pred_str = digits_to_label(torch.argmax(y_pred, 1)[0])
+        y_perturbed_pred_str = digits_to_label(torch.argmax(y_perturbed_pred, 1)[0])
+        # print("gt:", y_str)
+        # print("pred:", y_pred_str)
+        # print("perturbed:", y_perturbed_pred_str)
+        # print("target:", y_target_str)
+
+        axs[i, 0].set_title(f"actual: {y_str} pred: {y_pred_str}")
+        axs[i, 0].imshow(torch.permute(x[0], (1, 2, 0)).detach().cpu().numpy())
+        axs[i, 0].axis("off")
+        axs[i, 1].set_title(f"permuted pred: {y_perturbed_pred_str}")
+        axs[i, 1].imshow(
+            torch.permute(x_perturbed[0], (1, 2, 0)).detach().cpu().numpy()
+        )
+        axs[i, 1].axis("off")
+
+    fig.savefig("image")
+
+
+def evaluate_fgsm(model, loader, epsilon, device):
+    original_accuracy = 0
+    perturbed_accuracy = 0
+    n = 0
+    model.eval()
+    model.to(device)
+    for x, y in tqdm(loader):
+        x = x.to(device)
+        y = y.to(device)
+        x_perturbed = x.clone().detach().requires_grad_(True)
+        y_pred = model(x_perturbed)
+
+        x_perturbed = fast_gradient_sign(model, x_perturbed, y, epsilon=epsilon)
+        y_perturbed_pred = model(x_perturbed)
+
+        original_accuracy += torch.sum(
+            torch.all((torch.argmax(y_pred, dim=1) == y), dim=-1).to(torch.float32)
+        )
+        perturbed_accuracy += torch.sum(
+            torch.all((torch.argmax(y_perturbed_pred, dim=1) == y), dim=-1).to(
+                torch.float32
+            )
+        )
+        n += x.shape[0]
+
+    return (original_accuracy / n).item(), (perturbed_accuracy / n).item()
+
+
+def plot_epsilon_dependency(model, loader, device):
+    epsilons = np.logspace(-3, -0.7, 10)
+    accuracies = []
+    for epsilon in epsilons:
+        _, accuracy = evaluate_fgsm(model, loader, epsilon, device)
+        accuracies.append(accuracy * 100)
+
+    plt.xlabel("Epsilon")
+    plt.ylabel("Accuracy (%)")
+    plt.title("Accuracy vs Epsilon")
+    plt.plot(epsilons, accuracies)
+    plt.savefig("acc_vs_eps.png")
+
+
 if __name__ == "__main__":
-    PATH = "cleaned_checkpoint.pt"
+    PATH = "cleaned_checkpoint_1.pt"
+    DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("using: ", DEVICE)
 
     model, loss_fn = load_checkpoint(PATH)
 
-    # user code
-    dataset = CaptchaDataset(
-        "akashguna/large-captcha-dataset",
-        transform=T.Compose(
-            (
-                T.Resize((224, 224)),
-                T.ToTensor(),
-                # T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-            )
-        ),
+    transforms = T.Compose(
+        (
+            T.Resize((224, 224)),
+            T.ToTensor(),
+        )
     )
-    loader = DataLoader(dataset, batch_size=1, shuffle=True)
-    x, y = next(iter(loader))
-    x_perturbed = x.clone().detach().requires_grad_(True)
-    # y_pred = model(x)
-    y_target = y
-    # y_target = dataset.label_to_digits("aaaaa").unsqueeze(0)
-    y_pred = peek_inside_model(model, x_perturbed)
-    # for i in range(10):
-    #     loss = loss_fn(peek_inside_model(model, x_perturbed), y_target)
-    #     model.zero_grad()
-    #     loss.backward()
-    #     print(loss)
-    #     print(x_perturbed.requires_grad)
-    #     # x_perturbed = x_perturbed + 0.05 * torch.sign(x_perturbed.grad)
-    #     x_perturbed = x_perturbed - 0.04 * x_perturbed.grad
-    # x_perturbed = torch.clamp(x_perturbed, 0, 1)
-    x_perturbed = fast_gradient_sign(model, x_perturbed, y)
-    y_perturbed_pred = model(x_perturbed)
+    # user code
+    (train_set, val_set, test_set), _ = split_dataset(
+        get_combined_dataset(transforms),
+        [0.8, 0.1, 0.1],
+        "combined_splits_indices.cache",
+    )
+    test_subset = Subset(test_set, range(100))
+    loader = DataLoader(test_subset, batch_size=16, shuffle=True)
 
-    y_str = digits_to_label(y[0])
-    y_target_str = digits_to_label(y_target[0])
-    y_pred_str = digits_to_label(torch.argmax(y_pred, 1)[0])
-    y_perturbed_pred_str = digits_to_label(torch.argmax(y_perturbed_pred, 1)[0])
-    print("gt:", y_str)
-    print("pred:", y_pred_str)
-    print("perturbed:", y_perturbed_pred_str)
-    print("target:", y_target_str)
-    fig, axs = plt.subplots(1, 2)
-    axs[0].set_title(f"original: {y_pred_str}")
-    axs[0].imshow(torch.permute(x[0], (1, 2, 0)).detach().cpu().numpy())
-    axs[0].axis("off")
-    axs[1].set_title(f"{y_target_str}: {y_perturbed_pred_str}")
-    axs[1].imshow(torch.permute(x_perturbed[0], (1, 2, 0)).detach().cpu().numpy())
-    axs[1].axis("off")
-
-    fig.savefig("image")
+    # visualize(model, test_loader, 4, DEVICE)
+    # orig, pert = evaluate_fgsm(model, loader, 0.1, DEVICE)
+    # print("orig: ", orig, "pert: ", pert)
+    plot_epsilon_dependency(model, loader, DEVICE)
